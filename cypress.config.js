4de432c1b6edb4b9bd2061e12c1ac779807b7156
Jehangiri <165431455+KSJehangiri@users.noreply.github.com>
2026-02-07 19:26:45 -0500

    second commit: Cypress Premium App Test

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://green-flamingo-854862.hostingersite.com/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Add JUnit reporter
+      on('after:run', (results) => {
+        // nothing extra needed here for simple setup
+      });
+    },
+    reporter: 'mocha-junit-reporter',
+    reporterOptions: {
+      mochaFile: 'cypress/results/results-[hash].xml', // this is where reports will be saved
+      toConsole: true
     },
   },
 });
